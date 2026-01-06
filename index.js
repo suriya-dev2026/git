39f29fe727be1cefe9723a178efb05c9ff3f0fbd
demo <suriyasundarrajan340@gmail.com>
2026-01-06 14:45:20 +0530

    feature

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-console.log("Welcome to the git course")
-for(var i=0;i<5;i++)
-{
-    console("code io feature")
-=======
 console.log("Welcome to the git playlist")
 for(var i=0;i<5;i++){
     console.log("code io");
->>>>>>> main
 }
